274c32a1e93c8bef50e858f43f3249d11e995a34
ErkinovTolib <tolibjonerkinov015@gmail.com>
2022-07-01 07:50:48 +0500

    While04

--- While04.py
+++ While04.py
@@ -1,9 +1,16 @@
 def main(s):
     """
-    A variable of type str is given. Find how many uppercase letters there are and return.
+    A variable of type str is given. Find how many uppercase letters there are 
+    and return.
     Args:
         s: str
     Returns:
         int: return answer
     """
-    return
+    idx = 0
+    ans = 0
+    while idx<len(s):
+        ans += s[idx].isupper()
+        idx += 1
+    return ans
+print(main("CodeSchool"))
